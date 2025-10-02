3d9fdf1994d2f20afca17607ae360fcf7c61af8a
maeosha <mochkauha123@gmail.com>
2025-10-02 19:31:16 +0300

    feat: add enums: BloodGroup, Gender and RhesusFactor

---
+++ Clinic/Clinic.Models/Enums/BloodGroup.cs
@@ -0,0 +1,10 @@
+namespace Clinic.Models.Enums
+{
+    public enum BloodGroup
+    {
+        First,   
+        Second, 
+        Third, 
+        Fourth
+    }
+}
---
+++ Clinic/Clinic.Models/Enums/Gender.cs
@@ -0,0 +1,8 @@
+namespace Clinic.Models.Enums
+{
+    public enum Gender
+    {
+        Male,  
+        Female
+    }
+}
