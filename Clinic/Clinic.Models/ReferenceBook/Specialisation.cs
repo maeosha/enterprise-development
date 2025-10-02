44a437e8f4572f99b0ed0be645ea6729d7e1d0bb
maeosha <mochkauha123@gmail.com>
2025-10-02 19:32:40 +0300

    feat: add reference book Specialisation

---
+++ Clinic/Clinic.Models/ReferenceBook/Specialisation.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Clinic.Models.ReferenceBooks
+{
+    public class Specialisation
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
